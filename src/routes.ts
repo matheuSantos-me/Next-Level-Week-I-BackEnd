6931ced3e674c369209052d23a184e2e85ef738b
Matheus Santos <matheus.santos@server.signainfo.com.br>
2020-08-27 23:33:53 -0300

    docs: Create route list items

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import knex from './database/connection'
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  res.send('OIOIOIOIOIOI')
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*')
+
+  return res.json(items)
 })
 
 export default routes
